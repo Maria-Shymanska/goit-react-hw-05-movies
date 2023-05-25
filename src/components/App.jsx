f48e50e01d9d07d0051c97412a66614ec7bd8360
Maria Shymanska <114136940+Maria-Shymanska@users.noreply.github.com>
2023-05-25 11:42:18 +0200

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,2 +1,26 @@
-// import { Routes, Route } from 'react-router-dom';
-// import { lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Container from './Layout/Layout';
+import { lazy } from 'react';
+
+const HomePage = lazy(() => import('../views/HomePage'));
+const SearchMovies = lazy(() => import('../views/SearchMovies'));
+const MovieDetailsPage = lazy(() => import('../views/MovieDetailsPage'));
+const Cast = lazy(() => import('../views/Cast'));
+const Reviews = lazy(() => import('../views/Reviews'));
+const NotFoundView = lazy(() => import('../views/NotFoundView'));
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Container />}>
+        <Route index element={<HomePage />} />
+        <Route path="movies" element={<SearchMovies />} />
+        <Route path="movies/:movieId" element={<MovieDetailsPage />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+      </Route>
+      <Route path="*" element={<NotFoundView />} />
+    </Routes>
+  );
+}
