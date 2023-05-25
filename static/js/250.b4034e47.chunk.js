e0f93dee52bd84faf6605db16497a54341dfbf9b
Maria-Shymanska <114136940+Maria-Shymanska@users.noreply.github.com>
2023-05-25 10:21:34 +0000

    Deploying to gh-pages from @ Maria-Shymanska/goit-react-hw-05-movies@10e2b7259399ebaff2e1da448ba657d30207498f 🚀

--- static/js/250.b4034e47.chunk.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[250],{24:function(){}}]);
--- static/js/339.f7f9ce1a.chunk.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[339],{339:function(){}}]);
